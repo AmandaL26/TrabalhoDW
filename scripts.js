c292fb50825654b2721d98dc9c14f4d8bc07b869
mariaa-paula <maria.freitas5@estudante.ifms.edu.br>
2025-09-25 17:08:55 -0400

    remover campos de entrada do formulário e corrigir erros de digitação no script

--- scripts.js
+++ scripts.js
@@ -1,66 +1,76 @@
-const form = document.getElementById('form');
-const usename = document.getElementById('username');
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const passwordConfirmation = document.getElementById('password-confirmation');
+const form= document.getElementById('form');
+const username= document.getElementById('username');
+const email= document.getElementById('email');
+const password= document.getElementById('password');
+const passawoedConfimation= document.getElementById('password-confirmation');
+const cpf= document.getElementById('cpf');
+const cnpj= document.getElementById('cnpj');
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
 
-    checkInputs()
+    checkImput();
 })
 
-function checkInputs() {
+ function checkImput(){
     const usernameValue = username.value;
     const emailValue = email.value;
     const passwordValue = password.value;
-    const passwordConfirmationValue = passwordConfirmation.value;
+    const passawoedConfimationValue = passawoedConfimation.value;
+    const cpfValue = cpf.value;
+    const cnpjValue = cnpj.value;
 
-    if (usernameValue === '') {
-        setErrorFor(username, 'O nome de usuário é obrigatório');
-    } else {
-        setSuccessFor(username);
-    }
+   if (usernameValue === ''){
+      setErrorFor(username, 'O nome de usuário é obrigatório');
+   } else {
+    setSuccessFor(username);
+   }
 
-    if (emailValue === '') {
-        setErrorFor(email, 'O email é obrigatório');
-    } else {
-        setSuccessFor(email);
-    }
+   if (emailValue === ''){
+    setErrorFor(email, 'O email é obrigatório!');
+   } else {
+  setSuccessFor(email);
+   }
 
-    if (passwordValue === '') {
-        setErrorFor(password, 'A senha é obrigatório');
-    } else {
-        setSuccessFor(password);
-    }
+   if (passwordValue === ''){
+     setErrorFor(password, 'Senha obrigatória');
+   } else {
+     setSuccessFor(password);
+   }
 
-    if (passwordConfirmationValue === '') {
-        setErrorFor(passwordConfirmation, 'A confirmaçãosenha é obrigatório');
-    } else {
-        setSuccessFor(passwordConfirmation);
-    }
-}
+   if (passawoedConfimationValue === ''){
+     setErrorFor(passawoedConfimation, 'Confirmação de senha obrigatória');
+   } else {
+     setSuccessFor(passawoedConfimation);
+   }
 
+   if (cpfValue === ''){
+     setErrorFor(cpf, 'O CPF é obrigatório');
+   } else {
+     setSuccessFor(cpf);
+   }
 
-function setErrorFor(input, message) {
+   if (cnpjValue === ''){
+     setErrorFor(cnpj, 'O CNPJ é obrigatório');
+   } else {
+     setSuccessFor(cnpj);
+   }
+ }
+
+ function setErrorFor(input, message){
     const formControl = input.parentElement;
     const small = formControl.querySelector('small');
 
     small.innerText = message;
 
-    formControl.className = 'form-control error'
-}
-
-function setSuccessFor(input) {
-    const formControl = input.parentElement
-
-    formControl.className = 'form-control success'
-}
+    formControl.className = 'form-control error';
+ }
 
 
+ function setSuccessFor(input){
+    const formControl = input.parentElement;
+    formControl.className = 'form-control success';
 
-function checkEmail(email) {
-    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
-        email
-    );
-}  
+ }
+
+
